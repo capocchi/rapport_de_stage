dd764b6b14d950e0c59b3b1af7efa87fbd228219
kappablanka <theo.piacentini@gmail.com>
2023-04-20 22:08:50 +0200

    ajout d'un script de build améliorable pour le latex

---
+++ build_latex.ps1
@@ -0,0 +1,32 @@
+# export of drawio files to pdf
+
+$DIR_DRAWIO = "./figures/drawio"
+
+$DrawIoFiles = Get-ChildItem $DIR_DRAWIO *.drawio -File
+
+foreach ($file in $DrawIoFiles) {
+    & "C:/Program Files/draw.io/draw.io.exe" --crop -x -f pdf $file.FullName -o ./figures -v
+}
+
+$DIR_EPS = "./figures/eps"
+
+$EpsFiles = Get-ChildItem $DIR_EPS *.eps -File
+
+Write-Host "aaaaaaaaaaaaaaaaaaaa"
+
+foreach ($file in $EpsFiles) {
+    epstopdf $file.FullName
+    Copy-Item "./figures/eps/$($file.BaseName).pdf"  -Destination ./figures -Force
+    Remove-Item "./figures/eps/$($file.BaseName).pdf" -Force 
+}
+
+$DIR_SVG = "./figures/svg"
+
+$SvgFiles = Get-ChildItem $DIR_SVG *.svg -File
+
+foreach ($file in $SvgFiles) {
+    inkscape $file.FullName --export-type=pdf --export-filename="./figures/$($file.BaseName).pdf"
+}
+
+latexmk -pdf -c
+
